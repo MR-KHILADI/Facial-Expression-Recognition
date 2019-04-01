2c994212e405a841e325099abf0433464caef74b
v-k-e <40107589+v-k-e@users.noreply.github.com>
2019-03-31 18:14:09 -0700

    Add files via upload
    Added options to
    - train on original data
    - balance the training data

--- ExpressionRecognition.py
+++ ExpressionRecognition.py
@@ -3,7 +3,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 import numpy.linalg as LA
+from sklearn.metrics import confusion_matrix
+from imblearn.over_sampling import RandomOverSampler, SMOTE
+from collections import Counter
 
+# Take a 2304 long vector and plot it as a 48x48 image
 def vectortoimg(v,show=True):
     plt.imshow(v.reshape(48, 48),interpolation='None', cmap='gray')
     plt.axis('off')
@@ -15,7 +19,7 @@ def vectortoimg(v,show=True):
 # Returns:   Mean vector of X
 #            Eigen vectors, as rows, with max on top
 #            Principal components of X
-def performPCA(X, T, showScatterPlot=False):
+def performPCA(X, T, showScatterPlot=False, showSeparatePlots=False):
     # PERFORM the XZCVP process
     # X -> Z
     muX = np.mean(X, axis=0) # get means of columns, by setting axis=0
@@ -43,15 +47,36 @@ def performPCA(X, T, showScatterPlot=False):
         
         for digit in np.unique(T):
             ix = np.where(T == digit)
-            ax.scatter(reducedP[ix,0], reducedP[ix,1], s=5, c=mapOfColors[digit], label='Digit '+str(digit), linewidths=0, marker="o")
+            ax.scatter(reducedP[ix,0], reducedP[ix,1], s=5, c=mapOfColors[digit], label='Class: '+mapOfEmotion[digit], linewidths=0, marker="o")
         ax.set_aspect('equal')
         plt.legend(markerscale=5.0)
         plt.show()
 
+    # Draw SCATTER PLOT (separate for each class)
+    if (showSeparatePlots):
+        plt.close('all')
+        fig = plt.figure()
+        
+        reducedP = P[:, 0:2]
+        
+        for digit in np.unique(T):
+            ix = np.where(T == digit)
+            ax = fig.add_subplot(plotLoc[digit], facecolor='black')
+            ax.scatter(reducedP[ix,0], reducedP[ix,1], s=5, c=mapOfColors[digit], label='Class: '+mapOfEmotion[digit], linewidths=0, marker="o")
+            ax.set_title(mapOfEmotion[digit])
+            ax.set_xbound(-6000, 6000)
+            ax.set_ybound(-3500, 3500)
+        ax.set_aspect('equal')
+        #plt.legend(markerscale=5.0)
+        plt.show()
+
     return muX, V, P
     
+# For a vector 'x', returns the pdf, in the form of log(pdf)
+#   Optimized as it takes 'inverse of cov. matrix' and 
+#   'log of det. of cov. matrix', and as such, does not 
+#   have to recompute them 
 def pdfLogOpt(x, mu, cvMtxInv, logDet):
-
     diffFromMean = (x - mu)
     powOfE = diffFromMean.dot(cvMtxInv)
     powOfE = powOfE.dot(diffFromMean.T)
@@ -61,8 +86,13 @@ def pdfLogOpt(x, mu, cvMtxInv, logDet):
 
     return (numerPowOfE-denomPowOfE)
 
+# Build a bayesian classifier, given the training data (X)
+#   and the training labels (T). It will return 
+#   1. the training data split up by classes
+#   2. counts for each class
+#   3. mean vector for each class
+#   4. cov. matrix for each class
 def buildBayesianClassifier(X, T):
-    
     # split training data into class-specific sets
     XByClass = []
     nByClass = []
@@ -89,6 +119,10 @@ def buildBayesianClassifier(X, T):
 
     return XByClass, nByClass, muByClass, cvMtxByClass
 
+# Make predictions on given 'queries', using a supplied bayesian model 
+#   which is specified in terms of (count, mean vector, cov. matrix) for 
+#   each class, and also the 'label' for each class.
+#   Currently, returns the predicted class label for each query
 def predictWithBayesian(nByClass, muByClass, cvMtxByClass, classLabels, queries):
     # for each QUERY, compute bayesian numbers and produce prob
     predLabel = []
@@ -110,7 +144,7 @@ def predictWithBayesian(nByClass, muByClass, cvMtxByClass, classLabels, queries)
             likelyClass = np.argmax(estLogByClass)
             predLabel.append(classLabels[likelyClass])
 
-            if (np.remainder(iQuery,100) == 0):
+            if (np.remainder(iQuery,1000) == 0):
                 print('Done with query# ', iQuery)
 
     return predLabel, predProb
@@ -119,6 +153,15 @@ def predictWithBayesian(nByClass, muByClass, cvMtxByClass, classLabels, queries)
 ### Classes: 0=Angry, 1=Disgust, 2=Fear, 3=Happy, 4=Sad, 5=Surprise, 6=Neutral
 mapOfEmotion = {0:"Angry", 1:"Disgust", 2:"Fear", 3:"Happy", 4:"Sad", 5:"Surprise", 6:"Neutral"}
 mapOfColors = {0: [1,0,0,0.5], 1: [0,1,0,0.5], 2: [0,0,1,0.5], 3:[1,1,0,0.5], 4:[0,1,1,0.5], 5:[1,0,1,0.5], 6:[0.5,0.5,0.5,0.5]}
+plotLoc = {0: 241, 1: 242, 2: 243, 3:244, 4:245, 5:246, 6:247}
+
+balanceTheTrainingSet = False
+augmentRotatedImages = False
+trainOnOriginalComponents = False
+showSingleScatter = True
+showSeparateScatter = True
+predictOnPublicTestSet = False
+predictOnTrainingSet = True
 
 ## Data Loading and Splitting
 dataFolderPath = "D:\\python_ML\\Project\\"
@@ -133,32 +176,51 @@ Test_labels=Test_data.iloc[:,0].values
 Train_data=Train_data.drop(columns='X1').values
 Test_data=Test_data.drop(columns='X1').values
 
-# for x in np.arange(5):
-#     nImgToDraw = random.randint(0, X.shape[0]-1)
-#     print(mapOfEmotion[Train_labels[nImgToDraw]])
-#     vectortoimg(Train_data[nImgToDraw])
-
 # downsample from 64bit int to an 8bit uint
 Train_data = Train_data.astype(dtype=np.uint8)
 Train_labels = Train_labels.astype(dtype=np.uint8)
 
-# AUGMENT THE TRAINING DATA WITH ROTATIONS
-otherDataFiles = ["fer2013_10toleft.csv", "fer2013_10toright.csv", "fer2013_3toleft.csv", "fer2013_3toright.csv", "fer2013_6toleft.csv", "fer2013_6toright.csv"]
+if (balanceTheTrainingSet):
+    ros = RandomOverSampler(random_state=0)
+    Train_data, Train_labels = ros.fit_resample(Train_data, Train_labels)
+    #Train_data, Train_labels = SMOTE().fit_resample(Train_data, Train_labels)
+    print(sorted(Counter(Train_labels).items()))
 
-nTrgVectors = Train_data.shape[0] 
-for otherDataFile in otherDataFiles:
-    data = pd.read_csv(dataFolderPath + otherDataFile)
-    data = data.values.astype(dtype=np.uint8)
+# OPTIONAL, AUGMENT the TRAINING data with ROTATIONS
+if (augmentRotatedImages):
+    otherDataFiles = []
+    #otherDataFiles = ["fer2013_3toleft.csv", "fer2013_3toright.csv", "fer2013_6toleft.csv", "fer2013_6toright.csv"]
+    #otherDataFiles = ["fer2013_3toleft.csv", "fer2013_3toright.csv", "fer2013_6toleft.csv", "fer2013_6toright.csv", "fer2013_10toleft.csv", "fer2013_10toright.csv"]
     
-    newLabels = data[:,0]
-    newData = data[:,1:]
-
-    # 'data' is missing the first training image
-    Train_data = np.vstack( (Train_data, newData[0:nTrgVectors]) ) 
-    Train_labels = np.hstack( (Train_labels, newLabels[0:nTrgVectors]) )
+    nTrgVectors = Train_data.shape[0] 
+    for otherDataFile in otherDataFiles:
+        data = pd.read_csv(dataFolderPath + otherDataFile)
+        data = data.values.astype(dtype=np.uint8)
+        
+        newLabels = data[:,0]
+        newData = data[:,1:]
+    
+        # 'data' is missing the first training image
+        Train_data = np.vstack( (Train_data, newData[0:nTrgVectors]) ) 
+        Train_labels = np.hstack( (Train_labels, newLabels[0:nTrgVectors]) )
+    
+# OPTIONAL try on regular data (i.e., original components)
+if (trainOnOriginalComponents):
+    TrainByClass, nByClass, muByClass, cvMtxByClass = buildBayesianClassifier(Train_data, Train_labels)
+    
+    # now, predict on the PRIVATE test set with Bayesian classifier
+    Test_data = Test_data.astype(dtype=np.uint8)
+    predLabelTest, predProbTest = predictWithBayesian(nByClass, muByClass, cvMtxByClass, np.unique(Train_labels), Test_data)
+    res = (predLabelTest==Test_labels)
+    print('Accuracy over PRIVATE TEST SET is ', res.sum()/len(Test_labels))
+    
+    # also, predict on the TRAINING set with Bayesian classifier
+    predLabelTrg, predProbTrg = predictWithBayesian(nByClass, muByClass, cvMtxByClass, np.unique(Train_labels), Train_data)
+    res = (predLabelTrg==Train_labels)
+    print('Accuracy over TRAINING SET is ', res.sum()/len(Train_labels))
 
 # Peform PCA on the training data
-muTrain, V, PTrain = performPCA(Train_data, Train_labels)
+muTrain, V, PTrain = performPCA(Train_data, Train_labels, showSingleScatter, showSeparateScatter)
 
 # 2 gives us accuracy of 25.57%
 # 19 gives us accuracy of 32.79%
@@ -215,28 +277,34 @@ PTest = np.dot(ZTest, reducedV.T)
 
 predLabelTest, predProbTest = predictWithBayesian(nByClass, muByClass, cvMtxByClass, np.unique(Train_labels), PTest)
 
-res = (predLabelTest==Test_labels)
-print('Accuracy over PRIVATE TEST SET is ', res.sum()/len(Test_labels))
+testResCM = confusion_matrix(Test_labels, predLabelTest)
+print('Confusion matrix is:\n', testResCM)
+acc = testResCM.diagonal().sum() / testResCM.sum()
+print('Accuracy over PRIVATE TEST SET is ', acc)
+print('PPV values for the classes are:', testResCM.diagonal() / testResCM.sum(axis=0))
+print('Sensitivity values for the classes are:', testResCM.diagonal() / testResCM.sum(axis=1))
 
-# # now, predict on the PUBLIC test set with Bayesian classifier
-# Test_data=data[data['X2306']=='PublicTest'].drop(columns='X2306')
-# Test_labels=Test_data.iloc[:,0].values
-# Test_data=Test_data.drop(columns='X1').values
-# Test_data = Test_data.astype(dtype=np.uint8)
-# 
-# ZTest = Test_data - muTrain
-# PTest = np.dot(ZTest, reducedV.T)
-# 
-# predLabelTest, predProbTest = predictWithBayesian(nByClass, muByClass, cvMtxByClass, np.unique(Train_labels), PTest)
-# 
-# res = (predLabelTest==Test_labels)
-# print('Accuracy over PUBLIC TEST SET is ', res.sum()/len(Test_labels))
+# OPTIONAL, predict on the PUBLIC test set with Bayesian classifier
+if (predictOnPublicTestSet):
+    data=pd.read_csv(dataFolderPath + "FaceRecognition.csv")
+    data=data.drop(columns='Unnamed: 0')
+    Test_data=data[data['X2306']=='PublicTest'].drop(columns='X2306')
+    Test_labels=Test_data.iloc[:,0].values
+    Test_data=Test_data.drop(columns='X1').values
+    Test_data = Test_data.astype(dtype=np.uint8)
+    
+    ZTest = Test_data - muTrain
+    PTest = np.dot(ZTest, reducedV.T)
+    
+    predLabelTest, predProbTest = predictWithBayesian(nByClass, muByClass, cvMtxByClass, np.unique(Train_labels), PTest)
+    res = (predLabelTest==Test_labels)
+    print('Accuracy over PUBLIC TEST SET is ', res.sum()/len(Test_labels))
 
-
-# also, predict on the training set with Bayesian classifier
-# 360 PC dimensions-> 76.61%
-nSamplesToTry = 28709*7
-predLabelTrg, predProbTrg = predictWithBayesian(nByClass, muByClass, cvMtxByClass, np.unique(Train_labels), reducedP[0:nSamplesToTry])
-
-res = (predLabelTrg==Train_labels[0:nSamplesToTry])
-print('Accuracy over TRAINING SET is ', res.sum()/nSamplesToTry)
+# OPTIONAL, predict on the TRAINING set with Bayesian classifier
+# 360 PC dimensions-> 76.61% (with original set of 28709)
+# 360 PC dimensions-> 54% (with original+rotated set of 28709*7)
+if (predictOnTrainingSet):
+    nSamplesToTry = len(reducedP)
+    predLabelTrg, predProbTrg = predictWithBayesian(nByClass, muByClass, cvMtxByClass, np.unique(Train_labels), reducedP[0:nSamplesToTry])
+    res = (predLabelTrg==Train_labels[0:nSamplesToTry])
+    print('Accuracy over TRAINING SET is ', res.sum()/nSamplesToTry)
